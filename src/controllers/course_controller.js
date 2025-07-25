932086dc585e7f1187343c13e8cec619bc8145b7
cemrehasirci <cecemrehasirci@hotmail.com>
2025-07-25 17:34:01 +0300

    course 1/3

--- src/controllers/course_controller.js
+++ src/controllers/course_controller.js
@@ -2,14 +2,54 @@ const CourseSchema = require('../models/course.js')
 
 const getCourse = async (req, res) => {
     try {
-        const getCours = await CourseSchema.find(req.body);
-        if (getCours == "") {
+        const getCourse = await CourseSchema.find(req.body);
+        if (getCourse == "") {
             return res.status(500).json({message: "Henüz bir kursumuz nbulunmamakta..."})
         }
-        res.status(200).json({getCours});
+        res.status(200).json({getCourse});
     } catch (error) {
         return res.status(500).json({message: error.message})
     }
+};
+
+const createCourse = async (req, res) => {
+  try {
+    const newCourse = await CourseSchema.create(req.body);
+    res.status(201).json({
+      newCourse,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: "Course oluşturulamadı!" });
+  }
+};
+
+const updateCourse = async (req, res) => {
+    try {
+    const { id } = req.params;
+    const updateCourse = await PostSchema.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).json({
+      updateCourse,
+    });
+  } catch {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteCourse = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCourse = await CourseSchema.findByIdAndDelete(id);
+    if (!deletedCourse) {
+      return res.status(404).json({ message: "Course bulunamadı" });
+    }
+    res.status(201).json({
+      message: `${deletedCourse.id} id'li course başarıyla silindii...`,
+    });
+  } catch {
+    return res.status(500).json({ message: error.message });
+  }
 };
 
 
@@ -19,4 +59,5 @@ const getCourse = async (req, res) => {
 
 
 
-module.exports = { getCourse }
+
+module.exports = { getCourse, createCourse, updateCourse, deleteCourse}
--- src/routers/post_router.js
+++ src/routers/post_router.js
@@ -1,9 +1,11 @@
 const express = require('express');
-const { getCourse } = require('../controllers/course_controller.js');
-const auth = require('../middleware/auth_middleware.js')
+const { getCourse, createCourse, updateCourse, deleteCourse} = require('../controllers/course_controller.js');
+//const auth = require('../middleware/auth_middleware.js')
 const router = express.Router();
 
 router.get('/getCourse', getCourse);
-
+router.post('/createCourse', createCourse);
+router.patch('/updateCourse/:id', updateCourse);
+router.delete('/deleteCourse/:id', deleteCourse)
 
 module.exports = router
